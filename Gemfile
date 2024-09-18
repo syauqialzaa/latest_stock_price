276c51f7a4fa1c6fe9e382fdbdbd12d46d36d606
Alza Syauqi <syauqialza7@gmail.com>
2024-09-18 10:06:01 +0700

    init: first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in latest_stock_price.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/latest_stock_price.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "latest_stock_price/version"
+
+module LatestStockPrice
+  class Error < StandardError; end
+  # Your code goes here...
+end
